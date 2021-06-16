056472874a96e78e656af58fe3df96e83f2ec04c
Aditi3 <aditi.gvc@gmail.com>
2021-06-16 09:48:20 +0530

    chore: Add Dice Swift UI

---
+++ DiceSwiftUIExample/DiceSwiftUIExample/DiceSwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceSwiftUIExampleApp.swift
+//  DiceSwiftUIExample
+//
+//  Created by Aditi Agrawal on 16/06/21.
+//
+
+import SwiftUI
+
+@main
+struct DiceSwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
